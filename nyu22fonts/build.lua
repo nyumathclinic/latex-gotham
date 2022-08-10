56bdec82eda2edeec1e64904ef1c2b3af11b4dbc
Matthew Leingang <leingang@nyu.edu>
2022-08-10 07:10:25 -0400

    Tagging version 0.10.0

--- nyu22fonts/build.lua
+++ nyu22fonts/build.lua
@@ -2,8 +2,8 @@
 -- Matthew Leingang, 22022-08-05
 
 bundle = "Gotham"
-bundleversion = "0.9.1"
-bundledate    = "2022-08-05"
+bundleversion = "0.10.0"
+bundledate    = "2022-08-10"
 module = "nyu22fonts"
 
 maindir = ".."
