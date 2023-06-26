287e8aa9f08b1ec3e5a90133e38ffd54cfca9bff
Matthew Leingang <leingang@nyu.edu>
2023-06-26 08:55:54 -0400

    Set the standard test engine to luatex

--- xcolor-nyu22/build.lua
+++ xcolor-nyu22/build.lua
@@ -13,24 +13,8 @@ typesetdeps = { maindir .. "/nyu22fonts"}
 supportdir = "support"
 typesetexe = "lualatex"
 
--- Root directory of the TDS structure for the module to be installed into.
--- Mentally prepend the path to the correct texmf tree THEN either "tex" (for
--- includes), "doc" (for documentation), or "source" (for source files).
--- tdsroot = "latex"
-
--- The "luatex" engine produces some very minor differences (like, a single
--- space in a \kern line).  One way around that is to disable the luatex engine:
--- (uncomment to activate)
---
--- checkengines = { "pdftex", "xetex" }
---
--- Another way is to save test logs twice (on command line)
---
---     $ l3build save test
---     $ l3build save -e luatex test
---
--- This creates both "test.tlg" and "test.luatex.tlg" in testfiles.
--- Engine-specific tlg files are only compared with the specific engines.
+stdengine = "luatex"
+checkdeps = {maindir .. "/nyu22fonts"}
 
 dofile(maindir .. "/build-config.lua")
 
